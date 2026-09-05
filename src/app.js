2909097275ad6982115e6406c999c4dc757d534e
vedantmane82@gmail.com <vedantmane82@gmail.com>
2026-09-05 19:16:11 +0530

    Practice CRUD APIs

--- src/app.js
+++ src/app.js
@@ -18,8 +18,46 @@ app.post("/notes", async(req,res)=>{
 
 })
 
+app.get("/notes", async(req,res)=>{
+    const notes = await noteModel.find()//returns an array
+
+    res.status(200).json({
+        message: "Notes Fetched Successfully",
+        notes: notes
+    })
+})
+
+app.delete("/notes/:id",async (req,res) =>{
+
+    const id = req.params.id
+
+    await noteModel.findOneAndDelete({
+        _id: id
+    })
+
+    res.status(200).json({
+        message: "Note Deleted successfully"
+    })
 
 
+
+
+
+})
+
+
+
+app.patch("/notes/:id", async(req,res)=>{
+    const id = req.params.id
+    const description = req.body.description
+
+    await noteModel.findOneAndUpdate({_id:id},{description:description})
+
+    res.status(200).json({
+        message:"Note Deleted successfully"
+    })
+    
+})
 
 
 module.exports = app;
